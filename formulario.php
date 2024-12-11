ffeb2b91d10bfbb5a2290666b9ae9baee050ae37
samirrol92 <samiyt92@gmail.com>
2024-12-10 22:59:53 -0600

    Update formulario.php

--- formulario.php
+++ formulario.php
@@ -4,14 +4,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $apellido = $_POST['apellido'];
     $matricula = $_POST['matricula'];
 
-    // Ruta del archivo donde se guardarán los datos
     $archivo = 'datos.csv';
-
-    // Crear o abrir el archivo y agregar los datos
-    $file = fopen($archivo, 'a');
-    fputcsv($file, [$nombre, $apellido, $matricula]);
-    fclose($file);
-
-    echo "Datos guardados correctamente";
+    
+    if (($file = fopen($archivo, 'a')) !== false) {
+        fputcsv($file, [$nombre, $apellido, $matricula]);
+        fclose($file);
+        echo json_encode(['status' => 'success']);
+    } else {
+        echo json_encode(['status' => 'error']);
+    }
 }
 ?>
